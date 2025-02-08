97d49316936e031d6863b6ae7460e93b8ba6584f
GabrielC248 <gabrielccoelho10@gmail.com>
2025-02-08 11:31:16 -0300

    Configurações iniciais

---
+++ Tarefa_PWM.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+#include "hardware/pwm.h"
+#include "hardware/clocks.h"
+
+int main()
+{
+
+    stdio_init_all();
+
+    while (true) {
+        
+    }
+}
